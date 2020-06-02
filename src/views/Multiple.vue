fb6988136bcb37035bd516b8f1eaa3d390508d49
zhangbo75 <zhangbo75@meituan.com>
2020-06-02 11:41:30 +0800

    words

--- src/views/Multiple.vue
+++ src/views/Multiple.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="page">
-    <ul>
+    <ul class="keys">
+      <li v-for="(item,i) in keys" :key="i">{{item}}</li>
+    </ul>
+    <ul class="words">
       <li v-for="(item,i) in words" :key="i">
         <span>{{i+1}}</span>
         <strong>{{item}}</strong>
       </li>
     </ul>
+    <a href="javascript:void(0)" class="btn-1" @click="randomKey">密码</a>
+    <a href="javascript:void(0)" class="btn-2" @click="randomWord">密码本</a>
   </div>
 </template>
 <script>
@@ -14,22 +19,38 @@ import {getMultipleWords} from '../words';
 export default {
   data(){
     return {
-      words:['词汇','词汇','词汇','词汇']
+      keys:[0,0,0],
+      words:['词汇','词汇','词汇','词汇'],
     };
   },
   mounted(){
-    this.newWords();
+    this.randomWord();
   },
   methods: {
-    newWords(){
+    randomWord(){
       this.words = getMultipleWords(4);
-    }
+    },
+    randomKey(){
+      const arr = [1,2,3,4];
+      arr.sort(() => Math.random() - 0.5); 
+      this.keys = arr.slice(0,3);
+    },
   },
 }
 </script>
 <style lang="scss" scoped>
-
-ul {
+ul.keys{
+  padding: 20px;
+  display:flex;
+  li{
+    flex:1;
+    height: 60px;
+    font-size: 35px;
+    line-height:60px;
+    text-align: center;
+  }
+}
+ul.words {
   position:fixed;
   top:50%;
   left:50%;
@@ -56,5 +77,26 @@ ul {
       text-align: center;
     }
   }
+}
+
+.btn-1 {
+  display:block;
+  width: 150px;
+  position:absolute;
+  bottom: 20px;
+  left:20px;
+  font-size:24px;
+  line-height: 50px;
+  background:#ddd;
+}
+.btn-2 {
+  display:block;
+  width: 150px;
+  position:absolute;
+  bottom: 20px;
+  right:20px;
+  font-size:24px;
+  line-height: 50px;
+  background:#ddd;
 }
 </style>
